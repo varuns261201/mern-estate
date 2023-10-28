4642cbbed20ec6201fbb5103ffcdaecb0177d476
Varun <svarun26dec@gmail.com>
2023-10-29 02:30:35 +0530

    completed signup ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import React from 'react'
 import SignIn from './pages/SignIn';
 import Home from './pages/Home';
-import SignUp from './pages/Signup';
+import SignUp from './pages/SignUp';
 import About from './pages/About';
 import Profile from './pages/Profile';
 import Header from './components/Header';
