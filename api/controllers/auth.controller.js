0d2595017c1695dc08a7bf0762fe6fa7eaaaaa4a
Varun <svarun26dec@gmail.com>
2023-10-29 20:46:56 +0530

    created sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
-// import { errorHandler } from "../utils/error.js";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -14,5 +15,24 @@ export const signup = async (req, res, next) => {
   } catch (error) {
     next(error);
     // next(errorHandler(550,'error from the function')) this error is created by us
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User Not Found!"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Invalid Password!"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET); //this is a token for authentication
+    const { password: pass, ...rest } = validUser._doc;
+    //we save token as cookie
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
   }
 };
